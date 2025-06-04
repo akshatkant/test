cecfa2eee0917ede200b4b5fb9cb8c643d59ab4e
Akshat Kant Thakur <68228350+akshatkant@users.noreply.github.com>
2025-06-05 00:04:15 +0530

    check

--- test.java
+++ test.java
@@ -2,6 +2,10 @@ package test;
 
 public class test {
     public static void main(String[] args) {
-        
+        if (args.length > 0) {
+            System.out.println("Hello, " + args[0] + "!");
+        } else {
+            System.out.println("Hello, World!");
+        }
     }
 }
